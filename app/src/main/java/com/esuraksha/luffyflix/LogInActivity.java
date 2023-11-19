3509bdec6293bee8c28c5388908a545c87dd1fc3
Vivek Kumar <62384505+Vnjvibhash@users.noreply.github.com>
2023-11-19 22:44:42 +0530

    Login Design

--- app/src/main/java/com/esuraksha/luffyflix/LogInActivity.java
+++ app/src/main/java/com/esuraksha/luffyflix/LogInActivity.java
@@ -3,12 +3,19 @@ package com.esuraksha.luffyflix;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LogInActivity extends AppCompatActivity {
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        login = findViewById(R.id.login);
+        login.setOnClickListener(v->{
+
+        });
     }
 }
